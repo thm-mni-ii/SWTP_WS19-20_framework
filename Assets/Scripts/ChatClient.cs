6bb5775f80d949b465d2bab058a46b132a538d65
mazm52 <muhamad.azem@mni.thm.de>
2019-10-31 11:16:29 +0100

    Chat Client/Server

---
+++ Assets/Scripts/ChatClient.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using UnityEngine;
+using Mirror;
+using System.Text;
+using UnityEngine.UI;
+
+
+public class ChatClient : MonoBehaviour
+{
+    Telepathy.Client client = new Telepathy.Client();
+	public int clientport= 7777;
+	public string ip = "localhost";
+	private String clientmsg = null;
+	private String chatmsg = null;
+	public int MaxMessages = 15;
+	private int msgnum = 0;
+	
+	
+
+    void Awake()
+    {
+        // update even if window isn't focused, otherwise we don't receive.
+        Application.runInBackground = true;
+
+        // use Debug.Log functions for Telepathy so we can see it in the console
+        Telepathy.Logger.Log = Debug.Log;
+        Telepathy.Logger.LogWarning = Debug.LogWarning;
+        Telepathy.Logger.LogError = Debug.LogError;
+		
+    }
+
+    void Update()
+    {
+        // client
+        if (client.Connected)
+        {
+            //if (Input.GetKeyDown(KeyCode.Space))
+              //  client.Send(new byte[]{0x1});
+
+            // show all new messages
+            Telepathy.Message msg;
+            while (client.GetNextMessage(out msg))
+            {
+                switch (msg.eventType)
+                {
+                    case Telepathy.EventType.Connected:
+                        Debug.Log("Client Connected on using ip: "+ ip);
+                        break;
+                    case Telepathy.EventType.Data:
+                        Debug.Log("Data: " + BitConverter.ToString(msg.data));
+						UpdateChat(msg.data);
+                        break;
+                    case Telepathy.EventType.Disconnected:
+                        Debug.Log("Disconnected");
+                        break;
+                }
+            }
+        }
+
+    }
+
+    void OnGUI()
+    {
+        // client
+		
+		GUI.enabled = !client.Connected;
+		if (GUI.Button(new Rect(0, 25, 120, 20), "LAN Client"))
+		{
+			client.Connect(ip, clientport);
+		}
+		ip = GUI.TextField(new Rect(260, 25, 120, 20),ip);
+
+        
+
+        GUI.enabled = client.Connected;
+        if (GUI.Button(new Rect(130, 25, 120, 20), "Disconnect Client"))
+            client.Disconnect();
+		
+		GUI.Box(new Rect(270, 100, 300, 400), chatmsg);
+		clientmsg = GUI.TextField(new Rect(130, 410, 120, 20),clientmsg);
+		if (GUI.Button(new Rect(300, 410, 120, 20), "send")){
+			
+			byte[] bytes = Encoding.ASCII.GetBytes(clientmsg);
+			client.Send(bytes);
+			
+		}
+
+
+        GUI.enabled = true;
+    }
+	
+	void UpdateChat(Byte[] data){
+	chatmsg += "\n" + BitConverter.ToString(data);
+	msgnum++;
+	if(msgnum >= MaxMessages)
+		chatmsg = chatmsg.Substring(chatmsg.IndexOf('\n') + 1);
+		
+	}
+
+    void OnApplicationQuit()
+    {
+        client.Disconnect();
+    }
+	
+ 
+}
---
+++ Assets/Scripts/ChatServer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using UnityEngine;
+using Mirror;
+using System.Text;
+using UnityEngine.UI;
+
+public class ChatServer : MonoBehaviour
+{
+
+    Telepathy.Server server = new Telepathy.Server();
+	public int port= 7777;
+	//public int MaxMessages = 15;
+	
+	
+
+    void Awake()
+    {
+        // update even if window isn't focused, otherwise we don't receive.
+        Application.runInBackground = true;
+
+        // use Debug.Log functions for Telepathy so we can see it in the console
+        Telepathy.Logger.Log = Debug.Log;
+        Telepathy.Logger.LogWarning = Debug.LogWarning;
+        Telepathy.Logger.LogError = Debug.LogError;
+		
+    }
+
+    void Update()
+    {
+
+        // server
+        if (server.Active)
+        {
+           // if (Input.GetKeyDown(KeyCode.Space)){
+             //   server.Send(1, new byte[]{0x2});
+			//}
+
+            // show all new messages
+            Telepathy.Message msg;
+            while (server.GetNextMessage(out msg))
+            {
+                switch (msg.eventType)
+                {
+                    case Telepathy.EventType.Connected:
+                        Debug.Log(msg.connectionId + " Connected");
+                        break;
+                    case Telepathy.EventType.Data:
+                        Debug.Log(msg.connectionId + " Data: " + BitConverter.ToString(msg.data));
+						server.Send(msg.connectionId,msg.data);
+                        break;
+                    case Telepathy.EventType.Disconnected:
+                        Debug.Log(msg.connectionId + " Disconnected");
+                        break;
+                }
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+
+        // server
+        GUI.enabled = !server.Active;
+        if (GUI.Button(new Rect(0, 50, 120, 20), "Start Server"))
+            server.Start(port);
+
+        GUI.enabled = server.Active;
+        if (GUI.Button(new Rect(130, 50, 120, 20), "Stop Server"))
+            server.Stop();
+
+        GUI.enabled = true;
+    }
+
+    void OnApplicationQuit()
+    {
+        server.Stop();
+    }
+}
