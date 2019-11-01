28459ee1f5df1dd73b2850238d81052faf2e55df
Mohamad Kattaa <53947632+MKattaa@users.noreply.github.com>
2019-11-01 15:56:17 +0100

    login portal with UI

---
+++ Assets/Scripts/ServerChatInputField.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ServerChatInputField : MonoBehaviour
+{
+    public ServerConfiguration instanceOfServer;
+    private InputField inputField;
+    public ServerChatTextField serverChatTextField;
+
+    private void Start()
+    {
+        inputField = GetComponent<InputField>();
+    }
+
+    public void ValueChanged()
+    {
+        if (inputField.text.Contains("\n"))
+        {
+            Debug.Log("Server ->" + " Data: " + inputField.text);
+            serverChatTextField.appendText("Server ->" + " Data: " + inputField.text);
+            instanceOfServer.SendToAll(Encoding.ASCII.GetBytes(inputField.ToString()));
+            inputField.text = string.Empty;
+            inputField.ActivateInputField();
+        }
+    }
+}
