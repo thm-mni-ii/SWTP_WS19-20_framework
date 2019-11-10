05e46f9ed7a6f545b3dfbcb52ab1275f0318d7f2
mazm52 <muhamad.azem@mni.thm.de>
2019-11-10 16:29:11 +0100

    clientList

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class Game
+{
+    private string GameName;
+    private List<UserInfo> PlayerList = new List<UserInfo>();
+    private int GameID;
+    private string ServerIP;
+    private UserInfo GameHost;
+
+
+	public Game()
+	{
+
+	}
+}
