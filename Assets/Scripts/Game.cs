7f90d2969557b70465b52f6123548c09cbd42138
mazm52 <muhamad.azem@mni.thm.de>
2019-11-12 16:36:50 +0100

    ServerList update

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,18 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
 
 [Serializable]
 public class Game
 {
-    private string GameName;
-    private List<UserInfo> PlayerList = new List<UserInfo>();
-    private int GameID;
-    private string ServerIP;
-    private UserInfo GameHost;
+    public string ip;
+    public ushort port;
+    public string title;
+    public ushort players;
+    public ushort capacity;
 
+    public int lastLatency = -1;
 
-	public Game()
-	{
+    public Game(string ip, ushort port, string title, ushort players, ushort capacity)
+    {
+        this.ip = ip;
+        this.port = port;
+        this.title = title;
+        this.players = players;
+        this.capacity = capacity;
 
-	}
+    }
 }
