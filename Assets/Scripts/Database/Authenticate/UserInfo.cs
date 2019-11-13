3090f363c7ab81b245fe0b3c7a5dfcd38aad35d5
Mohamad Kattaa <53947632+MKattaa@users.noreply.github.com>
2019-11-13 15:59:08 +0100

    add sources for login system and fix register button

---
+++ Assets/Scripts/Database/Authenticate/UserInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class UserInfo : MonoBehaviour
+{
+    public string userN;
+    public int userScore;
+    public string localId;
+
+    private void Awake()
+    {
+        userN = Databasemanagment_Register.playerName;
+       
+        localId = Databasemanagment_Register.localId;
+    }
+
+    private void Start()
+    {
+        userN = Databasemanagment_Register.playerName;
+       
+        localId = Databasemanagment_Register.localId;
+    }
+
+    public UserInfo() 
+    {
+        userN = Databasemanagment_Register.playerName;
+       
+        localId = Databasemanagment_Register.localId;
+    }
+}
---
+++ Assets/Scripts/Database/DatabaseManager/Databasemanagment_Register.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FullSerializer;
+using Proyecto26;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class Databasemanagment_Register : MonoBehaviour
+{
+
+    public InputField emailText;
+    public InputField usernameText;
+    public InputField passwordText;
+
+
+    UserInfo user = new UserInfo();
+
+    private string databaseURL = "https://mein-5d0b2.firebaseio.com/";
+    private string AuthKey = "AIzaSyCscgviTiGsQOjWxxJJ4cERbxycPo4OdCg";
+
+    public static fsSerializer serializer = new fsSerializer();
+
+    public static string playerName;
+    private string idToken;
+
+    public static string localId;
+
+    private string getLocalId;
+
+
+    private void Start()
+    {
+    }
+    
+    
+
+    private void PostToDatabase(bool emptyScore = false, string idTokenTemp = "")
+    {
+        if (idTokenTemp == "")
+        {
+            idTokenTemp = idToken;
+        }
+
+        UserInfo user = new UserInfo();
+
+
+
+        RestClient.Put(databaseURL + "/" + localId + ".json?auth=" + idTokenTemp, user);
+    }
+
+    
+
+    public void SignUpUserButton()
+    {
+        SignUpUser(emailText.text, usernameText.text, passwordText.text);
+    }
+
+    private void SignUpUser(string email, string username, string password)
+    {
+        string userData = "{\"email\":\"" + email + "\",\"password\":\"" + password + "\",\"returnSecureToken\":true}";
+        RestClient.Post<SignResponse>("https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=" + AuthKey, userData).Then(
+            response =>
+            {
+                string emailVerification = "{\"requestType\":\"VERIFY_EMAIL\",\"idToken\":\"" + response.idToken + "\"}";
+                RestClient.Post(
+                    "https://www.googleapis.com/identitytoolkit/v3/relyingparty/getOobConfirmationCode?key=" + AuthKey,
+                    emailVerification);
+                localId = response.localId;
+                playerName = username;
+                PostToDatabase(true, response.idToken);
+
+            }).Catch(error =>
+            {
+                Debug.Log(error);
+            });
+    }
+
+    
+
+    private void GetUsername()
+    {
+        RestClient.Get<UserInfo>(databaseURL + "/" + localId + ".json?auth=" + idToken).Then(response =>
+        {
+            playerName = response.userN;
+
+
+        });
+    }
+
+    
+}
