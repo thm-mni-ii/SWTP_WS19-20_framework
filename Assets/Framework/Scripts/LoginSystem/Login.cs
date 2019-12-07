59ef0e9b94e66f9b2e702a8c1845c1120b97a38d
mazm52 <muhamad.azem@mni.thm.de>
2019-12-07 14:32:57 +0100

    Merge

--- Assets/Framework/Scripts/LoginSystem/Login.cs
+++ Assets/Framework/Scripts/LoginSystem/Login.cs
@@ -21,6 +21,8 @@ public class Login : MonoBehaviour
     [SerializeField] private InputField rPass2;
     private GlobalManager globalCanvas;
     [SerializeField] private Text WarningMsg;
+    [SerializeField] private Text ResWarningMsg;
+    [SerializeField] private InputField ResEmail;
     [SerializeField] private Text regWarningMsg;
     private UserInfo user;
     private Chat chat;
@@ -112,13 +114,13 @@ public class Login : MonoBehaviour
                     WarningMsg.text = "please confirm your email";
                     break;
                 case 16:
-                    WarningMsg.text = "Password reset email sent successfully";
+                    ResWarningMsg.text = "Password reset email sent successfully";
                     break;
                 case 17:
-                    WarningMsg.text = "Error Send request was canceled";
+                    ResWarningMsg.text = "Error Send request was canceled";
                     break;
                 case 18:
-                    WarningMsg.text = "Could not send reset E-mail";
+                    ResWarningMsg.text = "Could not send reset E-mail";
                     break;
                 case 19:
                     report = 0;
@@ -212,6 +214,7 @@ public class Login : MonoBehaviour
                 }
                 else
                 {
+                    newUser.SendEmailVerificationAsync();
                     report = 15;
                 }
 
@@ -247,6 +250,11 @@ public class Login : MonoBehaviour
 
         if (rUsername.text != null && rUsername.text != "" && rEmail.text != null && rEmail.text != "" && rPass1.text != null && rPass1.text != "" && rPass2.text != null && rPass2.text != "")
         {
+            if (!rEmail.text.Contains("@"))
+            {
+                regWarningMsg.text = "Please enter your E-mail";
+                return;
+            }
 
 
             string u = null;
@@ -284,6 +292,12 @@ public class Login : MonoBehaviour
 
         if (rUsername.text != null && rUsername.text != "" && rEmail.text != null && rEmail.text != "" && rPass1.text != null && rPass1.text != "" && rPass2.text != null && rPass2.text != "")
         {
+
+            if (rPass1.text.Length < 6 || rPass2.text.Length < 6)
+            {
+                regWarningMsg.text = "Password must contain at least 6 Charachters";
+                return;
+            }
 
             if (string.Compare(rPass1.text, rPass2.text) != 0)
             {
@@ -362,6 +376,13 @@ public class Login : MonoBehaviour
 
 
     }
+    public void ResetButton()
+    {
+
+        globalCanvas.ToggleCanvas("forgot");
+
+
+    }
     public void backButton()
     {
 
@@ -373,14 +394,14 @@ public class Login : MonoBehaviour
     public void ResetPass()
     {
 
-        if (userName.text != null && userName.text != "")
+        if (ResEmail.text != null && ResEmail.text != "")
         {
-            if (!userName.text.Contains("@"))
+            if (!ResEmail.text.Contains("@"))
             {
-                WarningMsg.text = "Please enter your E-mail to reset password";
+                ResWarningMsg.text = "Please enter your E-mail to reset password";
                 return;
             }
-            auth.SendPasswordResetEmailAsync(userName.text).ContinueWith(task => {
+            auth.SendPasswordResetEmailAsync(ResEmail.text).ContinueWith(task => {
                 if (task.IsCanceled)
                 {
                     report = 17;
@@ -398,7 +419,7 @@ public class Login : MonoBehaviour
             });
         }else
         {
-            WarningMsg.text = "Please enter your E-mail to reset password";
+            ResWarningMsg.text = "Please enter your E-mail to reset password";
         }
 
 
