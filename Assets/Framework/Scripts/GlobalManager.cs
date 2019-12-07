65ca4a96306988c2610d183a674d7e4abd13dca7
MAlyasaqi <mohammad.alyasaqi@mni.thm.de>
2019-12-07 17:00:20 +0100

    rooms-name and magic-circel

--- Assets/Framework/Scripts/GlobalManager.cs
+++ Assets/Framework/Scripts/GlobalManager.cs
@@ -8,16 +8,12 @@ public class GlobalManager : MonoBehaviour {
 	public Canvas RegisterCanvas;
 	public Canvas LoginCanvas;
 	public Canvas ChatCanvas;
-    public Canvas PartyCanvas;
-    public Canvas ForgotCanvas;
 
-    // Use this for initialization
-    void Awake () {
+	// Use this for initialization
+	void Awake () {
 		RegisterCanvas.enabled = false;
 		LoginCanvas.enabled = true;
 		ChatCanvas.enabled  = false;
-        PartyCanvas.enabled = false;
-        ForgotCanvas.enabled = false;
 
     }
 	void Start () {
@@ -33,40 +29,21 @@ public class GlobalManager : MonoBehaviour {
 			RegisterCanvas.enabled = false;
 			LoginCanvas.enabled = true;
             ChatCanvas.enabled = false;
-            PartyCanvas.enabled = false;
-            ForgotCanvas.enabled = false;
 
 
         } else if (open == "register") {
 			RegisterCanvas.enabled = true;
 			LoginCanvas.enabled = false;
             ChatCanvas.enabled = false;
-            PartyCanvas.enabled = false;
-            ForgotCanvas.enabled = false;
 
 
         } else if (open == "chat") {
 			RegisterCanvas.enabled = false;
 			LoginCanvas.enabled = false;
 			ChatCanvas.enabled  = true;
-            PartyCanvas.enabled = true;
-            ForgotCanvas.enabled = false;
 
-        }
-        else if (open == "party")
-        {
-            PartyCanvas.enabled = true;
-
-        } else if (open == "forgot")
-        {
-            RegisterCanvas.enabled = false;
-            LoginCanvas.enabled = false;
-            ChatCanvas.enabled = false;
-            PartyCanvas.enabled = false;
-            ForgotCanvas.enabled = true;
-
-        }
-
-    }
+		}
+	
+	}
 
 }
--- Assets/Framework/Scripts/LoginSystem/Login.cs
+++ Assets/Framework/Scripts/LoginSystem/Login.cs
@@ -21,8 +21,6 @@ public class Login : MonoBehaviour
     [SerializeField] private InputField rPass2;
     private GlobalManager globalCanvas;
     [SerializeField] private Text WarningMsg;
-    [SerializeField] private Text ResWarningMsg;
-    [SerializeField] private InputField ResEmail;
     [SerializeField] private Text regWarningMsg;
     private UserInfo user;
     private Chat chat;
@@ -114,13 +112,13 @@ public class Login : MonoBehaviour
                     WarningMsg.text = "please confirm your email";
                     break;
                 case 16:
-                    ResWarningMsg.text = "Password reset email sent successfully";
+                    WarningMsg.text = "Password reset email sent successfully";
                     break;
                 case 17:
-                    ResWarningMsg.text = "Error Send request was canceled";
+                    WarningMsg.text = "Error Send request was canceled";
                     break;
                 case 18:
-                    ResWarningMsg.text = "Could not send reset E-mail";
+                    WarningMsg.text = "Could not send reset E-mail";
                     break;
                 case 19:
                     report = 0;
@@ -214,7 +212,6 @@ public class Login : MonoBehaviour
                 }
                 else
                 {
-                    newUser.SendEmailVerificationAsync();
                     report = 15;
                 }
 
@@ -250,11 +247,6 @@ public class Login : MonoBehaviour
 
         if (rUsername.text != null && rUsername.text != "" && rEmail.text != null && rEmail.text != "" && rPass1.text != null && rPass1.text != "" && rPass2.text != null && rPass2.text != "")
         {
-            if (!rEmail.text.Contains("@"))
-            {
-                regWarningMsg.text = "Please enter your E-mail";
-                return;
-            }
 
 
             string u = null;
@@ -292,12 +284,6 @@ public class Login : MonoBehaviour
 
         if (rUsername.text != null && rUsername.text != "" && rEmail.text != null && rEmail.text != "" && rPass1.text != null && rPass1.text != "" && rPass2.text != null && rPass2.text != "")
         {
-
-            if (rPass1.text.Length < 6 || rPass2.text.Length < 6)
-            {
-                regWarningMsg.text = "Password must contain at least 6 Charachters";
-                return;
-            }
 
             if (string.Compare(rPass1.text, rPass2.text) != 0)
             {
@@ -376,13 +362,6 @@ public class Login : MonoBehaviour
 
 
     }
-    public void ResetButton()
-    {
-
-        globalCanvas.ToggleCanvas("forgot");
-
-
-    }
     public void backButton()
     {
 
@@ -394,14 +373,14 @@ public class Login : MonoBehaviour
     public void ResetPass()
     {
 
-        if (ResEmail.text != null && ResEmail.text != "")
+        if (userName.text != null && userName.text != "")
         {
-            if (!ResEmail.text.Contains("@"))
+            if (!userName.text.Contains("@"))
             {
-                ResWarningMsg.text = "Please enter your E-mail to reset password";
+                WarningMsg.text = "Please enter your E-mail to reset password";
                 return;
             }
-            auth.SendPasswordResetEmailAsync(ResEmail.text).ContinueWith(task => {
+            auth.SendPasswordResetEmailAsync(userName.text).ContinueWith(task => {
                 if (task.IsCanceled)
                 {
                     report = 17;
@@ -419,7 +398,7 @@ public class Login : MonoBehaviour
             });
         }else
         {
-            ResWarningMsg.text = "Please enter your E-mail to reset password";
+            WarningMsg.text = "Please enter your E-mail to reset password";
         }
 
 
