9805140677ba60f273067a0ea4e5a50058f5d78f
Mohamad Kattaa <53947632+MKattaa@users.noreply.github.com>
2019-12-21 12:47:22 +0100

    update Build file

--- Assets/Framework/Scripts/PlayerMovement.cs
+++ Assets/Framework/Scripts/PlayerMovement.cs
@@ -8,46 +8,47 @@ namespace Mirror
      */
     public class PlayerMovement : NetworkBehaviour
     {
+        /// <summary>
+        /// *hier kommt noch was*
+        /// </summary>
         [SyncVar]
         public int index;
-
         /// <summary>
         /// manage the whole game. Hide and show the components.
         /// </summary>
         private GlobalManager globalCanvas;
-        
         /// <summary>
         /// Player score (helpful for the reward system)
         /// </summary>
         [SyncVar]
         public uint score;
-
         /// <summary>
         /// playerColor: to distinguish between player levels
         /// </summary>
         [SyncVar(hook = nameof(SetColor))]
         public Color playerColor = Color.black;
-        
         /// <summary>
         /// Unity clones the material when GetComponent<Renderer>().material is called
         /// Cache it here and destroy it in OnDestroy to prevent a memory leak
         /// </summary>
         Material cachedMaterial;
-
         /// <summary>
+        /// *hier kommt noch was*
         /// a reference to the client class which contains all of the client information it is used here to change the scene/canvas of the player,
         /// and to fix the problem where the player moves automaticly, when he types in the chat
         /// </summary>
-        /*hier kommt noch was*/
         private Client clientVar;
+        
+        [SerializeField] private Animator m_animator;
+        //[SerializeField] private Rigidbody m_rigidBody;
 
-
-        void Start()    /*hier kommt noch was*/
+        /// <summary>
+        /// *hier kommt noch was*
+        /// </summary>
+        void Start()
         {
             //globalCanvas = gameObject.GetComponent<GlobalManager>();
-            
             GameObject GM = GameObject.FindWithTag("GlobalManager");
-
             if (GM != null)
             {
                 globalCanvas = GM.GetComponent<GlobalManager>();
@@ -55,19 +56,33 @@ namespace Mirror
             var sphereCollider = gameObject.AddComponent<SphereCollider>();
             clientVar = globalCanvas.GetComponent<Client>();
         }
+        
+        public void Initialize(GameObject character)
+        {
+            m_animator = character.GetComponent<Animator>();
+            //m_rigidBody = character.GetComponent<Rigidbody>();
+        }
 
-        /**
-         * Used from Mirror /*hier kommt noch was*
-         */
+        
+        void Awake()
+        {
+            if(!m_animator) { gameObject.GetComponent<Animator>(); }
+            //if(!m_rigidBody) { gameObject.GetComponent<Animator>(); }
+        }
+
+        /// <summary>
+        /// Used from Mirror /*hier kommt noch was*
+        /// </summary>
+        /// <param name="color"></param>
         void SetColor(Color color)
         {
             if (cachedMaterial == null) cachedMaterial = GetComponent<Renderer>().material;
             cachedMaterial.color = color;
         }
-
-        /**
-         * Used from Mirror /*hier kommt noch was*
-         */
+        
+        /// <summary>
+        /// Used from Mirror /*hier kommt noch was*
+        /// </summary>
         void OnDisable()
         {
             if (isLocalPlayer)
@@ -78,51 +93,88 @@ namespace Mirror
             }
         }
 
-        /**
-         * Used from Mirror /*hier kommt noch was*
-         */
+        /// <summary>
+        /// Used from Mirror /*hier kommt noch was*
+        /// </summary>
         void OnDestroy()
         {
             Destroy(cachedMaterial);
         }
-
+        /// <summary>
+        /// *hier kommt noch was*
+        /// </summary>
         CharacterController characterController;
-
+        
+        /// <summary>
+        /// 
+        /// </summary>
         public override void OnStartLocalPlayer()
         {
             base.OnStartLocalPlayer();
-
             characterController = GetComponent<CharacterController>();
-
             Camera.main.transform.SetParent(transform);
-            Camera.main.transform.localPosition = new Vector3(0f, 3f, -8f);
+            Camera.main.transform.localPosition = new Vector3(0f, 3f, -7f);
             Camera.main.transform.localEulerAngles = new Vector3(10f, 0f, 0f);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         [Header("Movement Settings")]
         public float moveSpeed = 8f;
+        /// <summary>
+        /// 
+        /// </summary>
         public float turnSpeedAccel = 10f;
+        /// <summary>
+        /// 
+        /// </summary>
         public float turnSpeedDecel = 10f;
+        /// <summary>
+        /// 
+        /// </summary>
         public float maxTurnSpeed = 150f;
 
+        /// <summary>
+        /// 
+        /// </summary>
         [Header("Jump Settings")]
         public float jumpSpeed = 0f;
+        /// <summary>
+        /// 
+        /// </summary>
         public float jumpFactor = .025F;
 
+        public bool wasGrounded = false;
+
+        /// <summary>
+        /// 
+        /// </summary>
         [Header("Diagnostics")]
         public float horizontal = 0f;
+        /// <summary>
+        /// 
+        /// </summary>
         public float vertical = 0f;
+        /// <summary>
+        /// 
+        /// </summary>
         public float turn = 0f;
+        /// <summary>
+        /// 
+        /// </summary>
         public bool isGrounded = true;
+        /// <summary>
+        /// 
+        /// </summary>
         public bool isFalling = false;
 
-        /**
-         * Update player position /*hier kommt noch was*
-         */
+        /// <summary>
+        /// Update player position /*hier kommt noch was*
+        /// </summary>
         void Update()
         {
             if (!isLocalPlayer) return;
-
             horizontal = Input.GetAxis("Horizontal");
             vertical = Input.GetAxis("Vertical");
 
@@ -138,28 +190,41 @@ namespace Mirror
                 turn = 0f;
 
             if (!isFalling && Input.GetKey(KeyCode.Space) && (isGrounded || jumpSpeed < 1))
+            {
                 jumpSpeed += jumpFactor;
+                /*if (!wasGrounded && isGrounded)
+                {
+                    m_animator.SetTrigger("Land");
+                }
+
+                if (!isGrounded && wasGrounded)
+                {
+                    m_animator.SetTrigger("Jump");
+                }*/
+                m_animator.SetTrigger("Jump");
+            }
             else if (isGrounded)
+            {
                 isFalling = false;
+            }
             else
             {
                 isFalling = true;
                 jumpSpeed = 0;
+                m_animator.SetTrigger("Land");
+
             }
         }
 
-        /**
-         * Used from Mirror /*hier kommt noch was*
-         */
+        /// <summary>
+        /// Used from Mirror /*hier kommt noch was*
+        /// </summary>
         void FixedUpdate()
         {
-            
             if (!isLocalPlayer || characterController == null) return;
-
-            if (clientVar.clientMessageTF.isFocused) return; 
-
+            if (clientVar.clientMessageTF.isFocused) return;
+            m_animator.SetBool("Grounded", isGrounded);
             transform.Rotate(0f, turn * Time.fixedDeltaTime, 0f);
-
             Vector3 direction = new Vector3(horizontal, jumpSpeed, vertical);
             direction = Vector3.ClampMagnitude(direction, 1f);
             direction = transform.TransformDirection(direction);
@@ -168,52 +233,52 @@ namespace Mirror
             if (jumpSpeed > 0)
                 characterController.Move(direction * Time.fixedDeltaTime);
             else
+            {
+                //Vector3 direction = camera.forward * vertical + camera.right * horizontal;
+                m_animator.SetFloat("MoveSpeed", direction.magnitude);
                 characterController.SimpleMove(direction);
-
+            }
             isGrounded = characterController.isGrounded;
         }
-
+        
+        /// <summary>
+        /// 
+        /// </summary>
         GameObject controllerColliderHitObject;
-
-
-        /**
-         * override methode
-         * 
-         * When the player stand on the "Magic Circle" :
-         * 
-         * change the game type of in the client variable accordingly
-         * and show the startgame canvas to allow players to join host a party and start a game
-         */
+        
+        /// <summary>
+        /// override methode
+        /// When the player stand on the "Magic Circle" :
+        ///
+        /// change the game type of in the client variable accordingly
+        /// and show the startgame canvas to allow players to join host a party and start a game
+        /// </summary>
+        /// <param name="other"></param>
         private void OnTriggerEnter(Collider other) /*hier kommt noch was*/
         {
             if (!isLocalPlayer || characterController == null) return;
-
-            if (other.gameObject.tag.Equals("MATH") || other.gameObject.tag.Equals("NTG") || other.gameObject.tag.Equals("OOP"))
+            if (other.gameObject.tag.Equals("MATH") || other.gameObject.tag.Equals("NTG") || other.gameObject.tag.Equals("OOP") || other.gameObject.tag.Equals("GDI") || other.gameObject.tag.Equals("RNAI"))
             {
                 clientVar.setgameType(other.gameObject.tag);
                 globalCanvas.ToggleCanvas("gameOn");
                 clientVar.updateStartGameUI();
             }
         }
-
-
-        /*
-         * override methode
-         * disable the startgame canvas when the player leave the "Magic Circle"
-         * 
-         */
+        
+        /// <summary>
+        /// override methode
+        /// disable the startgame canvas when the player leave the "Magic Circle"
+        /// </summary>
+        /// <param name="other"></param>
         private void OnTriggerExit(Collider other) /*hier kommt noch was*/
-    {
+        {
             if (!isLocalPlayer || characterController == null) return;
 
-            if (other.gameObject.tag.Equals("MATH") || other.gameObject.tag.Equals("NTG") || other.gameObject.tag.Equals("OOP"))
+            if (other.gameObject.tag.Equals("MATH") || other.gameObject.tag.Equals("NTG") || other.gameObject.tag.Equals("OOP") || other.gameObject.tag.Equals("GDI") || other.gameObject.tag.Equals("RNAI"))
             {
                 clientVar.setgameType(null);
                 globalCanvas.ToggleCanvas("gameOff");
             }
         }
-}
-
-
     }
-
+}
